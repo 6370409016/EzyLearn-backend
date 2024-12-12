740ee24ec0eeda0ae823a371acdb1d4c2ccb095f
Badal <surajbadal69@gmail.com>
2024-12-12 16:12:19 +0530

    update deleteuser route

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const validateToken = require('../middleware/validateTokenHandler');
 const { userRegister, userLogin, handleRefreshToken, logOut } = require('../controller/userController');
 const { getUsersDetails, deleteAUserById, getAUserDetails, updateUserDetails } = require('../controller/adminController');
 
@@ -11,8 +12,8 @@ router.post('/refresh', handleRefreshToken);
 router.post('/register', userRegister);
 router.post('/login', userLogin);
 router.post('/logout', logOut);
-router.post('/deleteuser/:id', deleteAUserById);
-router.post('/updateuser/:id', updateUserDetails);
+router.get('/deleteuser/:id', validateToken, deleteAUserById);
+router.post('/updateuser/:id', validateToken, updateUserDetails);
 router.post('/getuser/:email', getAUserDetails);
 
 
